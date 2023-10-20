19f1e9282de41c1fe4bf93fb1f0038e327656e3c
clonidine <lcly@duck.com>
2023-10-20 12:03:02 -0300

    mutable list

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 fun main() {
-    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val numbers = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    
+    numbers.add(11)
+    numbers.add(12)
 
     numbers.forEach {
         println("Element: $it")
