9e5c88901ffe146164aa5ccad58021e6c9b02b24
clonidine <lcly@duck.com>
2023-10-20 12:38:21 -0300

    class methods

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
-class Person(val name: String, val age: Int)
+class Person(val name: String, val age: Int) {
+    fun canAccess(): Boolean {
+        return this.age >= 18
+    }
+}
 
 fun main() {
     val person = Person("Maria", 12)
 
-    println("Name: ${person.name}, Age: ${person.age}")
+    println("Name: ${person.name}, Age: ${person.age}, Can access: ${person.canAccess()}")
 }
