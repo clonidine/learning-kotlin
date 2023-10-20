c0dc4effca759eec858f8487019f1aa478edd226
clonidine <lcly@duck.com>
2023-10-20 12:09:22 -0300

    set

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,7 @@
 fun main() {
-    val numbers = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    
-    numbers.add(11)
-    numbers.add(12)
+    val n = setOf("cherry", "cherry", "cherry")
 
-    numbers.forEach {
-        println("Element: $it")
+    n.forEach {
+        println(it)
     }
 }
